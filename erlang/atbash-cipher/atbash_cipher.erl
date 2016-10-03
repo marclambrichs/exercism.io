4a3355cc8321ef096e36a061e48df2a8c24d7c8b
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-10-03 19:57:54 +0200

    Simplified code.

--- erlang/atbash-cipher/atbash_cipher.erl
+++ erlang/atbash-cipher/atbash_cipher.erl
@@ -1,33 +1,24 @@
 -module(atbash_cipher).
 -export([decode/1, encode/1]).
 
--define(ABC, "abcdefghijklmnopqrstuvwxyz").
--define(CBA, lists:reverse(?ABC)).
-
 -spec atbash_cipher:decode(string()) -> string().
 decode(L) ->
-    encode(prepare(L), ?CBA, ?ABC).
+    prepare(L).
     
 -spec atbash_cipher:encode(string()) -> string().
 encode(L) ->
-    L1 = encode(prepare(L), ?ABC, ?CBA),
-    string:strip(re:replace(L1, "\\w{5}", "& ",[global,{return,list}])).
-
--spec atbash_cipher:encode(string(), integer(), integer()) -> string().
-encode(L, From, To) ->
-    lists:map(fun(X) ->
-                      case (X >= $a andalso X =< $z) of
-                          true ->
-                              lists:nth(string:str(From, [X]),To);
-                          false ->
-                              X
-                      end
-              end,
-              L ).
+    A1 = prepare(L),
+    A2 = re:replace(A1, "\\w{5}", "& ",[global,{return,list}]),
+    string:strip(A2).
 
 -spec atbash_cipher:prepare(string()) -> string().
 prepare(L) ->
-    lists:filter(fun(X) -> (X >= $a andalso X =< $z) orelse
-                           (X >= $0 andalso X =< $9) 
-                 end, 
-                 string:to_lower(L)).
+    A1 = re:replace(L, "\\W", "",[global,{return,list}]),
+    A2 = string:to_lower(A1),
+    lists:map(fun translate/1, A2).
+
+-spec atbash_cipher:translate(char()) -> char().
+translate(C) when C >= $a andalso C =< $z ->
+    $z-C+$a;
+translate(N) when N >= $0 andalso N =< $9 ->
+    N.
